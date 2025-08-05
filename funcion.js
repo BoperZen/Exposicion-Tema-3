1e522f7bca3d0ef4c2566b45db0e02996729785e
Andresq38 <a.quesadam308@gmail.com>
2025-08-05 00:47:17 -0600

    Codigo ejemplo ApiRest

--- funcion.js
+++ funcion.js
@@ -1,4 +1,43 @@
 // API Rest
+function LlamadoFetch() {
+    fetch('https://jsonplaceholder.typicode.com/posts/5')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('result-fetch').textContent = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            document.getElementById('result-fetch').textContent = 'Error: ' + error;
+        });
+}
+
+function LlamadoXHR() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users/3');
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            document.getElementById('result-xhr').textContent = JSON.stringify(JSON.parse(xhr.responseText), null, 2);
+        } else {
+            document.getElementById('result-xhr').textContent = 'Error: ' + xhr.status;
+        }
+    };
+    xhr.onerror = function () {
+        document.getElementById('result-xhr').textContent = 'Error de red';
+    };
+    xhr.send();
+}
+
+function LlamadoJquery() {
+    $.ajax({
+        url: 'https://jsonplaceholder.typicode.com/comments/7',
+        method: 'GET',
+        success: function(data) {
+            $('#result-jquery').text(JSON.stringify(data, null, 2));
+        },
+        error: function(xhr, status, error) {
+            $('#result-jquery').text('Error: ' + error);
+        }
+    });
+}
 
 // Web RESTful (CRUD)
 function Get() {
